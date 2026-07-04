769ef34181619d713886f73abe1e09caec933747
Eden <2330922534@qq.com>
2026-07-04 21:42:33 +0800

    Remove redundant files
    - review_news_list.php: covered by review_list.php, no pages link to it
    - images/banner.JPG: leftover from old layout, removed in frontend redesign
    - images/logo.GIF: leftover from old layout, logo is now text-based
    - news_upgrade.sql: functionality replaced by upgrade.php

--- news_upgrade.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE news ADD COLUMN summary text AFTER content;
-ALTER TABLE news ADD COLUMN thumbnail varchar(200) AFTER attachment;
-ALTER TABLE news ADD COLUMN is_top tinyint(1) DEFAULT 0 AFTER thumbnail;
